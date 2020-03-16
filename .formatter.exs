670515e2a19887fa72f9b569d52d713e81ec18c6
Natali Maximenko <natali.maximenko@gmail.com>
2020-03-16 18:52:53 +0300

    Add credo, update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,49 @@
 [
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  locals_without_parens: [
+    # Ecto
+
+    ## schema
+    field: :*,
+    belongs_to: :*,
+    has_one: :*,
+    has_many: :*,
+    many_to_many: :*,
+    embeds_one: :*,
+    embeds_many: :*,
+
+    ## migration
+    create: :*,
+    create_if_not_exists: :*,
+    alter: :*,
+    drop: :*,
+    drop_if_exists: :*,
+    rename: :*,
+    add: :*,
+    remove: :*,
+    modify: :*,
+    execute: :*,
+
+    ## query
+    from: :*,
+
+    # router
+    plug: :*,
+    get: :*,
+    post: :*,
+    pipe_through: :*,
+    resources: :*,
+    options: :*,
+    put: :*,
+    delete: :*,
+
+    ## config
+    socket: :*,
+    transport: :*,
+
+    ## controllers
+    action_fallback: :*
+  ]
 ]
--- mix.exs
+++ mix.exs
@@ -33,16 +33,17 @@ defmodule Euler.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:ecto_sql, "~> 3.3"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
       {:phoenix, "~> 1.4.15"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.3"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
