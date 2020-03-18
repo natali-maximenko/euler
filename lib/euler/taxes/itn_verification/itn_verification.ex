03c49e92a9766398a85fe796810f088a3c5f0561
Natali Maximenko <natali.maximenko@gmail.com>
2020-03-18 10:30:08 +0300

    Add ten numbers checker implementation

--- lib/euler/taxes/itn_verification/itn_verification.ex
+++ lib/euler/taxes/itn_verification/itn_verification.ex
@@ -1,16 +1,13 @@
 defmodule Euler.Taxes.ItnVerification do
   alias Euler.Taxes.ItnVerification.{TenNumbersChecker, TwelveNumbersChecker}
 
-  def verify(checksum) when is_integer(checksum) do
-    int_len = checksum |> Integer.to_charlist() |> length()
-
-    case int_len do
-      10 -> TenNumbersChecker.verify(checksum)
-      12 -> TwelveNumbersChecker.verify(checksum)
+  def verify(itn) when is_bitstring(itn) do
+    case String.length(itn) do
+      10 -> TenNumbersChecker.verify(itn)
+      12 -> TwelveNumbersChecker.verify(itn)
       _ -> {:error, "not ITN"}
     end
   end
 
-  def verify(checksum) when is_integer(checksum) and checksum > 0, do: {:ok, false}
-  def verify(_), do: {:error, "not integer"}
+  def verify(_), do: {:error, "not string"}
 end
--- test/euler/taxes/taxes_test.exs
+++ test/euler/taxes/taxes_test.exs
@@ -3,21 +3,39 @@ defmodule Euler.Taxes.TaxesTest do
 
   alias Euler.Taxes
 
-  describe "#verify_itn/1" do
+  describe "#verify_itn/1 with ten numbers" do
     test "with valid checksum" do
-      checksum = 732_897_853_530
+      checksum = "2366010808"
       {:ok, result} = Taxes.verify_itn(checksum)
       assert result
     end
 
     test "with invalid checksum" do
-      checksum = 732_897_853_531
+      checksum = "2366010809"
       {:ok, result} = Taxes.verify_itn(checksum)
       refute result
     end
 
     test "with invalid itn returns error with message" do
-      assert {:error, _message} = Taxes.verify_itn("1111")
+      assert {:error, _message} = Taxes.verify_itn(-1)
+    end
+  end
+
+  describe "#verify_itn/1 with twelve numbers" do
+    test "with valid checksum" do
+      checksum = "732897853530"
+      {:ok, result} = Taxes.verify_itn(checksum)
+      assert result
+    end
+
+    test "with invalid checksum" do
+      checksum = "732897853531"
+      {:ok, result} = Taxes.verify_itn(checksum)
+      refute result
+    end
+
+    test "with invalid itn returns error with message" do
+      assert {:error, _message} = Taxes.verify_itn(-1)
     end
   end
 end
